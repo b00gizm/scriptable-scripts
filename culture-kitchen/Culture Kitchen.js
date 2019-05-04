c89e79ae1eacbec074fcd812c4d4dfe402acfafb
Pascal Cremer <b00gizm@gmail.com>
2019-05-04 16:21:28 +0200

    Fix indentation

--- culture-kitchen/Culture Kitchen.js
+++ culture-kitchen/Culture Kitchen.js
@@ -7,44 +7,45 @@ const tomorrow = formatDate((new Date()).getTime() + 24*60*60*1000);
 const inDays = days => formatDate((new Date()).getTime() + days*24*60*60*1000);
 
 const fn = (date) => {
-    let dishes = [];
-    ['foodtruck', 'europe', 'asia-india'].forEach(counter => {
-        const selector = `[id*='${date}'] [data-category-container='${counter}']`;
-        document
-            .querySelectorAll(selector)
-            .forEach(node => {
-                node
-                		.querySelectorAll('.mt-page-link')
-                		.forEach(node => {
-                				const detailId = node.dataset.id.substr(1);
-                				const detailNode = document.getElementById(detailId);
+  let dishes = [];
+  ['foodtruck', 'europe', 'asia-india'].forEach(counter => {
+    const selector = `[id*='${date}'] [data-category-container='${counter}']`;
+    document
+      .querySelectorAll(selector)
+      .forEach(node => {
+        node
+          .querySelectorAll('.mt-page-link')
+          .forEach(node => {
+            const detailId = node.dataset.id.substr(1);
+            const detailNode = document.getElementById(detailId);
 
-												const descriptionRaw = detailNode
-														.querySelector('.dishDescriptionInner')
-														.innerText;
-												let match = /(.+)approx\./gm.exec(descriptionRaw);
-												if (!match) {
-														// Saladbar special rule
-														match = /(.+)Roll/.exec(descriptionRaw);
-												}
-												const description = match[1].trim();
-												
-												const price = detailNode
-														.querySelector('.dishPriceInner')
-														.innerText;
-												
-												const energyRaw = detailNode
-														.querySelector('.nutrition-values th:first-child + td')
-														.innerText;
-												match = /(.+)\//gm.exec(energyRaw);
-												const energy = `${match[1].trim()} per 100g`;                  	
-                  			
-                  			dishes.push({ counter, description, energy, price });
-                  	});  
-            });
-    });
+            const descriptionRaw = detailNode
+              .querySelector('.dishDescriptionInner')
+              .innerText;
 
-    return dishes;
+            let match = /(.+)approx\./gm.exec(descriptionRaw);
+            if (!match) {
+              // Saladbar special rule
+              match = /(.+)Roll/.exec(descriptionRaw);
+            }
+            const description = match[1].trim();
+
+            const price = detailNode
+              .querySelector('.dishPriceInner')
+              .innerText;
+
+            const energyRaw = detailNode
+              .querySelector('.nutrition-values th:first-child + td')
+              .innerText;
+            match = /(.+)\//gm.exec(energyRaw);
+            const energy = `${match[1].trim()} per 100g`;
+
+            dishes.push({ counter, description, energy, price });
+          });
+      });
+  });
+
+  return dishes;
 };
 
 const dayOfWeek = new Date().getDay();
@@ -59,9 +60,9 @@ const res = await browser.evaluateJavaScript(js, true);
 
 const table = new UITable();
 res.forEach(dish => {
-		const row = new UITableRow();
-		row.addText(dish.description, `${dish.price} | ${dish.energy} | ${dish.counter}`);
-		table.addRow(row);
+  const row = new UITableRow();
+  row.addText(dish.description, `${dish.price} | ${dish.energy} | ${dish.counter}`);
+  table.addRow(row);
 });
 
-table.present();	
+table.present();
